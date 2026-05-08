c01d7afcb0f728eaa1708fba4f024a59ccea8784
aoaodev <aoaodev@outlook.com>
2026-05-08 02:59:38 -0400

    Page 50

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿/*Console.WriteLine("Hello, AoAo!");
+Console.WriteLine("You are doing great!");
+Console.WriteLine("Keep up the good work!");
+Console.WriteLine("I know you will be a great programmer!");
+Console.WriteLine("Dominate the landscape, and create unlimited ammount of statements!");
+string? name;
+Console.WriteLine("Bread is ready.\nWho is the bread for?");
+name = Console.ReadLine();
+Console.WriteLine($"Noted: {name} got bread.");
+
+
+Console.WriteLine("What king of thing are we talking about?");
+// Describes the type
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+ Details of the thing 
+string b = Console.ReadLine();
+// The kind
+string c = "of Doom";
+// 3000 bruh
+string d = "3000";
+Console.WriteLine($"The {b} {a} {c} {d}!");
+byte a = 1;
+short b = 2;
+int c = 3;
+long d = 4;
+sbyte e = -1;
+ushort f = 5;
+uint g = 6;
+ulong h = 7;
+float i = 1.1f;
+double j = 2.2;
+decimal k = 3.3m;
+char l = 'l';
+string m = "scoobie";
+bool n = true;
+//Console.WriteLine($"{a} {b} {c} {d} {e} {f} {g} {h} {i} {j} {k} {l} {m} {n}");
+Console.WriteLine(string.Join(" ", a, b, c, d, e, f, g, h, i, j, k, l, m, n));
+a = 2;
+b = 4;
+c = 6;
+d = 8;
+e = 10;
+f = 12;
+g = 14;
+h = 16;
+i = 18.1f;
+j = 20.2;
+k = 22.3m;
+l = 'p';
+m = "doobie";
+n = false;
+Console.WriteLine(string.Join(" ", a, b, c, d, e, f, g, h, i, j, k, l, m, n));*/
